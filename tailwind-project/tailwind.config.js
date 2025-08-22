f02ef775c00ec53b77f71b902e90b52af6b9b556
Dan <dp4006@ship.edu>
2025-08-21 21:14:36 -0400

    Init commnit

---
+++ tailwind-project/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{html,js}'],
+  darkMode: media,
+  theme: {
+    extend: {
+      colors: {
+        //primary: '#1DA1F2',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
